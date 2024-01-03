83dcaf400c1b450df04a11b7c54316813dbb809a
NizarRestu <nizarrestuaji18@gmail.com>
2024-01-03 14:35:49 +0700

    sekeleton be

---
+++ src/main/java/com/sistem/sistem_data_pondok_be/SistemDataPondokBeApplication.java
@@ -0,0 +1,13 @@
+package com.sistem.sistem_data_pondok_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemDataPondokBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemDataPondokBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistem/sistem_data_pondok_be/SistemDataPondokBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistem.sistem_data_pondok_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemDataPondokBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
